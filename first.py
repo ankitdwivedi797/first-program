68c1156e89e3a60da8b8724724872a12c2c70e3b
ankit d <ankit.dwivedi797@gmail.com>
2025-12-27 15:29:49 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,20 @@
+# var1 = input("Enter value")
+# print(var1)
+# print(type(var1))
+# var1 = int(var1)
+# # print(type(var1))
+# #comment
+# '''first-program
+# jnfjbjbjhbfg
+# '''
+# if isinstance(var1,int):
+#     print("it is integer class")
+
+for i in range(0,10):
+    print(i)
+
+for i in "Hello":
+    print(i)
+
+for i in [1,2,3,4]:
+    print(i)
